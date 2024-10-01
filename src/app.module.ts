f6d8cb9945addf08cec3ade3268e135914a7c753
João Lopes <jamichalski49@gmail.com>
2024-10-01 19:05:19 -0300

    fix module cliente

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { produtoModule } from './produto/produto.module';
-import { ClienteModule } from './cliente/cliente.module';
+import { clienteModule } from './cliente/cliente.module';
 import { EnderecoModule } from './endereco/endereco.module';
 import { PedidoModule } from './pedido/pedido.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { FormaPagamentoModule } from './forma-pagamento/forma-pagamento.module';
 
 @Module({
-  imports: [produtoModule, ClienteModule, EnderecoModule, PedidoModule, CategoriaModule, FormaPagamentoModule],
+  imports: [produtoModule, clienteModule, EnderecoModule, PedidoModule, CategoriaModule, FormaPagamentoModule],
   controllers: [AppController],
   providers: [AppService],
 })
