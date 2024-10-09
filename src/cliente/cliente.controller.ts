a8121daa524922c48e0b292b2d8f9192a3bb7417
João Lopes <jamichalski49@gmail.com>
2024-10-08 21:01:26 -0300

    cliente controller fix

---
+++ src/cliente/cliente.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { clienteService } from './cliente.service';
+
+
+@Controller('cliente')
+export class clienteController {
+    constructor(private service: clienteService){}
+
+    @Post()
+    async createcliente(@Body() body: any){
+        return await this.service.createcliente(body)
+    }
+
+    @Get('findMany')
+    async findclientes(){
+        return await this.service.findclientes()
+    }
+
+    @Get(':id')
+    async findcliente(@Param('id') id: string){
+        return await this.service.findcliente(id)
+    }
+
+    @Put()
+    async updatecliente(@Body()body: any){
+        return await this.service.updatecliente(body)
+    }
+}
