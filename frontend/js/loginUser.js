25e0a3906edc2a258d0136973c02aff01809a5b0
Murilo Baduy <baduymurilo@gmail.com>
2024-10-14 19:00:14 -0300

    ajustes
    commit na main memo azar, minha branch ta zoada

--- frontend/js/loginUser.js
+++ frontend/js/loginUser.js
@@ -1,4 +1,6 @@
-document.getElementById('form-login').addEventListener('submit', async function (event) {
+document
+  .getElementById('form-login')
+  .addEventListener('submit', async function (event) {
     event.preventDefault();
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
@@ -6,24 +8,24 @@ document.getElementById('form-login').addEventListener('submit', async function 
     const data = { email, senha };
 
     try {
-        const response = await fetch('http://localhost:3000/cliente/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
+      const response = await fetch('http://localhost:3000/cliente/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
 
-        if (response.ok) {
-            const result = await response.json();
-            alert('Login realizado com sucesso!');
-            window.location.href = 'index.html';
-        } else {
-            const error = await response.json();
-            alert(`Erro: ${error.message}`);
-        }
+      if (response.ok) {
+        const result = await response.json();
+        alert('Login realizado com sucesso!');
+        window.location.href = 'sublimelogged.html';
+      } else {
+        const error = await response.json();
+        alert(`Erro: ${error.message}`);
+      }
     } catch (erro) {
-        console.error('Erro ao enviar o formulário:', erro);
-        alert('Ocorreu um erro ao fazer login. Por favor, tente novamente.');
+      console.error('Erro ao enviar o formulário:', erro);
+      alert('Ocorreu um erro ao fazer login. Por favor, tente novamente.');
     }
-});
+  });
