494db8a5b121f8748aaebe4612151e5d28737e6e
Murilo Baduy <baduymurilo@gmail.com>
2024-10-14 18:48:50 -0300

    arrumando uns bgls

---
+++ frontend/js/loginUser.js
@@ -0,0 +1,31 @@
+document
+  .getElementById('form-login')
+  .addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const data = { email, senha };
+
+    try {
+      const response = await fetch('http://localhost:3000/cliente/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        alert('Login realizado com sucesso!');
+        window.location.href = 'sublimelogged.html';
+      } else {
+        const error = await response.json();
+        alert(`Erro: ${error.message}`);
+      }
+    } catch (erro) {
+      console.error('Erro ao enviar o formulário:', erro);
+      alert('Ocorreu um erro ao fazer login. Por favor, tente novamente.');
+    }
+  });
